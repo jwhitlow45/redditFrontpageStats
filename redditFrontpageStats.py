bac0f68510380231889731b734447ccb2dddea07
jwhitlow45 <jwhitlow45@gmail.com>
2021-02-24 12:21:55 -0800

    feat:implemented basic structure for accessing reddit posts

---
+++ redditFrontpageStats.py
@@ -0,0 +1,16 @@
+import praw
+import pprint
+
+# Config containing personal use script and secret token
+from config import Client
+
+reddit = praw.Reddit(
+    client_id=Client.ID,
+    client_secret=Client.SECRET,
+    user_agent=Client.AGENT
+)
+
+subreddit = reddit.subreddit('all')
+
+for submission in subreddit.hot(limit=1):
+    pprint.pprint(vars(submission))
