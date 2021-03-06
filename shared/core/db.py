6debc2991c43c836edfd7bd040ec42fb846c5c55
jwhitlow45 <jwhitlow45@gmail.com>
2021-03-06 13:55:50 -0800

    bug:Columns contained type int instead of sqlalchemy type Integer

--- shared/core/db.py
+++ shared/core/db.py
@@ -10,30 +10,30 @@ meta=MetaData()
 
 posts = Table(
     'posts', meta,
-    Column('id', String, primary_key=True), # unique id 
-    Column('title', String),                # title 
-    Column('author', String),               # author 
-    Column('created_utc', Time),            # time of creation in utc
-    Column('score', int),                   # score 
-    Column('upvote_ratio', Float),          # upvote ratio (upvote/total votes)
-    Column('num_comments', int),            # number of comments on post
-    Column('num_crossposts', int),          # number of crossposts 
-    Column('total_awards_received', int),   # total number of received awards
-    Column('subreddit_name', String),       # name of subreddit (r/subreddit)
-    Column('subreddit_id', String),         # unique id of subreddit
-    Column('subreddit_subscribers', int),   # number of subscribers to subreddit
-    Column('domain', String),               # domain hosting content
-    Column('permalink', String),            # permalink to post
-    Column('url', String),                  # url of the content
-    Column('gilded', int),                  # number of total gildings 
-    Column('gilded_silver', int),           # number of silver awards
-    Column('gilded_gold', int),             # number of gold awards
-    Column('gilded_platinum', int),         # number of platinum awards
-    Column('is_locked', Boolean),           # is the post locked
-    Column('is_meta', Boolean),             # is the post meta
-    Column('is_nsfw', Boolean),             # is the post nsfw
-    Column('is_oc', Boolean),               # is the post oc
-    Column('is_self', Boolean),             # is the post a self post
-    Column('is_spoiler', Boolean),          # is the post a spoiler
-    Column('is_video', Boolean)             # is the post a video
+    Column('id', String, primary_key=True),     # unique id 
+    Column('title', String),                    # title 
+    Column('author', String),                   # author 
+    Column('created_utc', Time),                # time of creation in utc
+    Column('score', Integer),                   # score 
+    Column('upvote_ratio', Float),              # upvote ratio (upvote/total votes)
+    Column('num_comments', Integer),            # number of comments on post
+    Column('num_crossposts', Integer),          # number of crossposts 
+    Column('total_awards_received', Integer),   # total number of received awards
+    Column('subreddit_name', String),           # name of subreddit (r/subreddit)
+    Column('subreddit_id', String),             # unique id of subreddit
+    Column('subreddit_subscribers', Integer),   # number of subscribers to subreddit
+    Column('domain', String),                   # domain hosting content
+    Column('permalink', String),                # permalink to post
+    Column('url', String),                      # url of the content
+    Column('gilded', Integer),                  # number of total gildings 
+    Column('gilded_silver', Integer),           # number of silver awards
+    Column('gilded_gold', Integer),             # number of gold awards
+    Column('gilded_platinum', Integer),         # number of platinum awards
+    Column('is_locked', Boolean),               # is the post locked
+    Column('is_meta', Boolean),                 # is the post meta
+    Column('is_nsfw', Boolean),                 # is the post nsfw
+    Column('is_oc', Boolean),                   # is the post oc
+    Column('is_self', Boolean),                 # is the post a self post
+    Column('is_spoiler', Boolean),              # is the post a spoiler
+    Column('is_video', Boolean)                 # is the post a video
 )
