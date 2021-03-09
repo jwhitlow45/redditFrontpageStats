5d288cc3308e85a0f4b4407854b8f3318cebddca
jwhitlow45 <jwhitlow45@gmail.com>
2021-03-08 17:10:57 -0800

    feat:added function to convert alphanumeric string to decimal by interpretting string as base-36 number

--- shared/post/utils.py
+++ shared/post/utils.py
@@ -1,5 +1,34 @@
-# SQL primary keys cannot be of type String
-# This function converts the string containing 
-def post_id_to_integer():
+def post_id_to_int(post_id:str) -> int:
+    """
+        Creates a unique integer id from a post id by treating
+        the string as a base-36 number and converting it to decimal
 
-    pass
+        Arguments: post_id->string
+
+        Returns: dec_value->integer
+
+        Errors: ValueError
+    """
+    # Reverse string
+    post_id = post_id[::-1].lower()
+    # Value to keep track of power in 
+    dec_value = 0
+    # Iterate through post_id converting each number to decimal
+    for index, char in enumerate(post_id):
+        # Check if character contains non-alphanumeric character
+        if not char.isalpha() and not char.isnumeric():
+            raise ValueError(f'String {post_id} contains non-\
+                alphanumeric characters')
+
+        # Decimal representation of character
+        dec_equiv = 0
+        if char.isalpha():
+            dec_equiv = ord(char)-87
+        else:
+            dec_equiv = ord(char)-48
+        # Add dec_equiv while account for place to dev_value
+        dec_value = dec_value+(dec_equiv*(36**index))
+
+    return dec_value
+
+print(post_id_to_int('100'))
